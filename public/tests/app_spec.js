7b6f28fb1cb3ce825607326176c49f1e16577dc8
Tushar Pokle <tushar.pokle@gmail.com>
2016-07-02 18:16:17 +1000

    Added hash parameters

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -9,4 +9,13 @@ describe('LearnJS', function () {
         learnjs.showView('')
         expect($('.view-container .landing-view').length).toEqual(1)
     })
+
+    it('passes the hash view paramter to the view function', function () {
+        
+        spyOn(learnjs, 'problemView')
+        learnjs.showView('#problem-42')
+        expect(learnjs.problemView).toHaveBeenCalledWith('42')
+    })
+
+
 })
